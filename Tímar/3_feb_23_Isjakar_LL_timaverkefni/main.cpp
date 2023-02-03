f47c8ac11d96c26d4f373bfbbb5e011e67b4d97d
gestskoli <ges@tskoli.is>
2023-02-03 12:35:05 +0000

    main fyrir ísjaka keðjulista

--- Tímar/3_feb_23_Isjakar_LL_timaverkefni/main.cpp
+++ Tímar/3_feb_23_Isjakar_LL_timaverkefni/main.cpp
@@ -7,11 +7,21 @@ using namespace std;
 
 int main() {
     IsjakarRadad ir;
-    
 
-    Isjaki eyddur = ir.eydaIsjaka(23);
-    if(eyddur.getId() == -1) {
-        cout << "Fann ekki ísjaka með id 23!!\n";
+    Isjaki j1 = Isjaki(10, 20, 30);
+    ir.skraIsjaka(j1);
+    ir.skraIsjaka(11, 20, 29);
+    ir.skraIsjaka(12, 50, 29);
+    ir.skraIsjaka(13, 40, 29);
+    ir.skraIsjaka(14, 10, 29);
+    ir.skraIsjaka(15, 20, 29);
+    ir.prentaAllt();
+    int id = 12;
+    if(ir.eydaIsjaka(id)) {
+        cout << "Eyddi ísjaka nr. " << id << endl;
+    } else {
+        cout << "Fann ekki ísjaka nr. " << id << endl;
     }
 
+    return 0;
 }
