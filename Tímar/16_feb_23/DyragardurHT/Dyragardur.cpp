1ab7037054797dde3374437f3840756afbab0924
gestskoli <ges@tskoli.is>
2023-02-16 13:43:14 +0000

    Dyragardur HT upphaf

---
+++ Tímar/16_feb_23/DyragardurHT/Dyragardur.cpp
@@ -0,0 +1,57 @@
+#include "Dyragardur.h"
+
+int Dyragardur::hash(std::string nafn) {
+
+}
+
+Dyragardur::Dyragardur() {
+
+}
+
+Dyragardur::Dyragardur(int staerd) {
+
+}
+
+Dyragardur::Dyragardur(Dyragardur& hinn) {
+
+}
+
+Dyragardur& Dyragardur::operator=(Dyragardur& hinn) {
+
+}
+
+void Dyragardur::skraDyr(Dyr* dyrid); {
+
+}
+
+void Dyragardur::skraHund(int aldur, std::string nafn, int einkunn) {
+
+}
+
+void Dyragardur::skraKott(int aldur, std::string nafn, std::string eigandi) {
+
+}
+
+bool Dyragardur::eydaDyri(std::string nafn) {
+
+}
+
+void Dyragardur::skodaDyr(std::string nafn) {
+
+}
+
+void Dyragardur::prentaAllt() {
+
+}
+
+void Dyragardur::prentaHunda() {
+
+}
+
+void Dyragardur::prentaKetti() {
+
+}
+
+Dyragardur::~Dyragardur() {
+
+}
